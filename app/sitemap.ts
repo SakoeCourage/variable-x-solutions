fb778085f70135fcc7e14b1343cc0da11c2001e6
Sakoe Courage <csakoe@ecfatum.com>
2026-01-30 14:50:09 +0000

    Added Fav Icon

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://variablexsolutions.com",
+      url: "https://www.variablexsolutions.com",
       lastModified: new Date(),
     },
     {
-        url: "https://variablexsolutions.com/software-architecture",
+        url: "https://www.variablexsolutions.com/software-architecture",
         lastModified: new Date(),
       },
   ];
